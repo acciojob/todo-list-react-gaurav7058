607e3b7d73c099c7d837b9df975701778199c877
Create_first_repo <gauravchoundiye123@gmail.com>
2024-11-12 15:14:24 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ const App = () => {
     <div>
         <h1>To Do List</h1>
         <div className="add_tasks_section">
-          <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
+          <textarea name="" id="" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></textarea> 
           <button className="task" onClick={handleInput}>Add</button>
         </div>
         <div className="tasks_section">
@@ -30,9 +30,9 @@ const App = () => {
            userStoreData.length>0 && userStoreData.map((item)=>{
               return(
                 <div className="" key={item.id}>
-                  <ul>
-                    <li>{item.name} <button className="edit">edit</button><button onClick={()=>handleDelete(item.id)} className="delete">Delete</button></li>
-                  </ul>
+                  <span>{item.name}</span>
+                  <button className="edit">edit</button>
+                  <button onClick={()=>handleDelete(item.id)} className="delete">Delete</button>
                 </div>
               )
             })
